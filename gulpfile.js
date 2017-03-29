a7e5bd74ba25616d9d7cf5c57bca655b2960db5f
Polina <polina14.05@yandex.ru>
2017-03-29 23:00:05 +0300

    контр.точка

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -18,12 +22,28 @@ gulp.task("style", function() {
         browsers: [
           "last 2 versions"
         ]
+      }), mqpacker({
+        sort: true
       })
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
-
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      })
+    ]))
+    .pipe(gulp.dest("img"));
+});
 gulp.task("serve", ["style"], function() {
   server.init({
     server: ".",
